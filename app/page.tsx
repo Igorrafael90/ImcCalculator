a69a17868985eb9dacf10a9031bc76cf5fc76ede
Igor Rafael Eloi dos Santos <Igor.Eloi@fies.org.br>
2025-01-16 11:27:39 -0300

    Estetica Calculadora

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <>
       <main className="flex flex-col">
-        <header className="flex items-center justify-between bg-black text-white w-full h-10">
-          <h1>IMC Calculator</h1>
+        <header className="flex items-center justify-between bg-black text-white w-full h-10 rounded-sm">
+          <h1 className="text-3xl">IMC Calculator</h1>
           <ul className="flex text-white space-x-5">
             <li><h2>O que é?</h2></li>
             <li><h2>Para que serve?</h2></li>
@@ -27,7 +27,7 @@ export default function Home() {
             </div>
             <div></div>
         </section>
-        <section className="flex h-auto min-h-80 bg-black text-white w-full justify-between items-center">
+        <section className="flex h-auto min-h-80 bg-black text-white w-full justify-between items-center rounded-sm">
           <div></div>
           <div className="space-y-2 text-right w-1/2 ">
               <h1 className="text-4xl border-white border-b-2 self-end">Para que serve?</h1>
@@ -35,12 +35,15 @@ export default function Home() {
                 Phasellus iaculis arcu sed ipsum tincidunt, sed convallis urna suscipit. Donec pretium interdum viverra. Duis odio dolor, tristique eget ullamcorper at, bibendum in leo. Quisque consectetur urna nec nunc egestas, in commodo metus elementum. Proin in mollis turpis, iaculis sodales nisi. In lacinia vehicula vulputate. Nunc sit amet ligula non justo sagittis mollis. Aliquam erat volutpat. Phasellus turpis orci, consequat vitae risus id, consectetur condimentum ex. Aliquam vitae urna libero.</p>
             </div>
         </section>
-        <section>
-          <h1 className="text-4xl border-white border-b-2 w-72">Calculadora</h1>
-          <input type="checkbox" />
-          <input type="number" value={Idade} onChange={(e) => setIdade(e.target.value)}/>
-          <input type="number" value={Altura} onChange={(e) => setAltura(e.target.value)}/>
-          <input type="number" value={Peso} onChange={(e) => setPeso(e.target.value)}/>
+        <section className="flex flex-col items-center w-full h-auto min-h-80 space-y-1">
+          <h1 className="text-4xl border-black border-b-2 w-72 text-center">Calculadora</h1>
+          <div className="flex flex-wrap w-1/2 h-36 border-black border-4">
+            <label>Idade:<input className="w-32 border rounded-sm shadow-black shadow-sm" type="number" value={Idade} onChange={(e) => setIdade(e.target.value)}/></label>
+            <label>Altura:<input className="w-32 border rounded-sm shadow-black shadow-sm" type="number" value={Altura} onChange={(e) => setAltura(e.target.value)}/></label>
+            <label>Peso: <input className="w-32 border rounded-sm shadow-black shadow-sm" type="number" value={Peso} onChange={(e) => setPeso(e.target.value)}/></label>
+            <button className="w-24 h-10 rounded-md bg-black text-white">Calcular</button>
+            <p></p>
+          </div>
         </section>
       </main>
     </>
